c2cfaa0620faf35b7e385326c713559fb8367654
sannimdev <inegg.apps@gmail.com>
2022-01-25 21:29:41 +0900

    4.9 Authentication part Two

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import schema from './schema';
 
 const server = new ApolloServer({
     schema,
+    context: {
+        token:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjQzMTEyODM0fQ.UGJ6K7XY9JNyr2k-JeQAw7ZT4Rt_c9RCVOJhmf4IRaA',
+    },
 });
 
 const PORT = process.env.PORT;
