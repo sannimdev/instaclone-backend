6dfd8fe403d91725d535a414d9fe170c46819229
sannimdev <inegg.apps@gmail.com>
2022-01-25 20:39:04 +0900

    4.5 Login part Two and Refactor

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import client from '../client';
 
 export default {
@@ -46,6 +47,11 @@ export default {
                 };
             }
             // issue a token and send it to the user
+            const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+            return {
+                ok: true,
+                token,
+            };
         },
     },
 };
