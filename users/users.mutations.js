d4098069bfe09cbf6bb5c4a0cf82e20c58b32209
sannimdev <inegg.apps@gmail.com>
2022-01-25 20:12:00 +0900

    4.3 seeProfile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -4,22 +4,29 @@ import client from '../client';
 export default {
     Mutation: {
         createAccount: async (_, { firstName, lastName, username, email, password }) => {
-            // check if username or email are already on DB.
-            const existingUser = await client.user.findFirst({
-                where: { OR: [{ username }, { email }] },
-            });
-            // hash password
-            const uglyPassword = await bcrypt.hash(password, 10);
-            // save and return the user
-            return client.user.create({
-                data: {
-                    username,
-                    email,
-                    firstName,
-                    lastName,
-                    password: uglyPassword,
-                },
-            });
+            try {
+                // check if username or email are already on DB.
+                const existingUser = await client.user.findFirst({
+                    where: { OR: [{ username }, { email }] },
+                });
+                if (existingUser) {
+                    throw new Error('This username/email is already taken.');
+                }
+                // hash password
+                const uglyPassword = await bcrypt.hash(password, 10);
+                // save and return the user
+                return client.user.create({
+                    data: {
+                        username,
+                        email,
+                        firstName,
+                        lastName,
+                        password: uglyPassword,
+                    },
+                });
+            } catch (e) {
+                return e;
+            }
         },
     },
 };
