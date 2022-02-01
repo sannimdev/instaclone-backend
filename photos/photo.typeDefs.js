55eedf54f34b709b267518c36173fc5f8197468e
sannimdev <inegg.apps@gmail.com>
2022-02-01 20:13:48 +0900

    6.1 Upload Photo part One

---
+++ photos/photo.typeDefs.js
@@ -0,0 +1,21 @@
+import { gql } from 'apollo-server';
+
+export default gql`
+    type Photo {
+        id: String!
+        user: user!
+        file: String!
+        caption: String
+        hashtag: [Hashtag]
+        createdAt: String!
+        updatedAt: String!
+    }
+
+    type Hashtag {
+        id: String!
+        hashtag: String!
+        photo: [Photo]
+        createdAt: String!
+        updatedAt: String!
+    }
+`;
