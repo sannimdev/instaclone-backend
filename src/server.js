3013ad5933bcc37faf4d95f63d28f77be58f2539
sannimdev <inegg.apps@gmail.com>
2022-02-05 21:22:20 +0900

    7.7.1 Apollo Server 버전 업데이트 대응

--- src/server.js
+++ src/server.js
@@ -5,31 +5,57 @@ import express from 'express';
 import logger from 'morgan';
 import { typeDefs, resolvers } from './schema';
 import { getUser } from './users/users.utils';
+// here for Subscriptions
+import { createServer } from 'http';
+import { execute, subscribe } from 'graphql';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 
 const PORT = process.env.PORT;
 const startServer = async () => {
+    const app = express();
+    app.use(logger('tiny')); // logger 사용 선언 다음에 적어줘야 로깅이 된다.
+    app.use('/static', express.static('uploads'));
+    app.use(graphqlUploadExpress());
+    const httpServer = createServer(app);
+    const schema = makeExecutableSchema({ typeDefs, resolvers });
+    const subscriptionServer = SubscriptionServer.create(
+        {
+            schema,
+            execute,
+            subscribe,
+        },
+        {
+            server: httpServer,
+            path: '/graphql',
+        }
+    );
     const server = new ApolloServer({
         // schema를 직접 입력하는 것 대신에 typeDefs, resolvers 항목을 넘기면 File Upload 가능
-        typeDefs,
-        resolvers,
         // production 모드에서도 플레이그라운드 사용
         // introspection: true,
         // playground: true,
+        schema,
+        plugins: [
+            {
+                async serverWillStart() {
+                    return {
+                        async drainServer() {
+                            subscriptionServer.close();
+                        },
+                    };
+                },
+            },
+        ],
         context: async ({ req }) => {
             return {
                 loggedInUser: await getUser(req.headers.token),
             };
         },
     });
-
     await server.start();
-    const app = express();
-
-    app.use(logger('tiny')); // logger 사용 선언 다음에 적어줘야 로깅이 된다.
-    app.use('/static', express.static('uploads'));
-    app.use(graphqlUploadExpress());
     server.applyMiddleware({ app });
-    await new Promise((func) => app.listen({ port: PORT }, func));
+    await new Promise((func) => httpServer.listen({ port: PORT }, func));
     console.log(`🥤 Server is running on http://localhost:4000${server.graphqlPath}`);
 };
 
