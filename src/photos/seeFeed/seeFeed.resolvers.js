8c33966dc2d45bfad6cfff676f95cc1b2fe18191
sannimdev <inegg.apps@gmail.com>
2022-02-23 20:03:23 +0900

    15.10 Infinite Scrolling part One

--- src/photos/seeFeed/seeFeed.resolvers.js
+++ src/photos/seeFeed/seeFeed.resolvers.js
@@ -3,25 +3,30 @@ import { protectedResolver } from '../../users/users.utils';
 
 export default {
     Query: {
-        seeFeed: protectedResolver(async (_, __, { loggedInUser }) => {
-            console.log(loggedInUser);
-            const result = await client.photo.findMany({
+        seeFeed: protectedResolver((_, { offset = 0 }, { loggedInUser }) =>
+            client.photo.findMany({
+                take: 2,
+                skip: offset,
                 where: {
                     OR: [
                         {
                             user: {
-                                followers: { some: { id: loggedInUser.id } },
+                                followers: {
+                                    some: {
+                                        id: loggedInUser.id,
+                                    },
+                                },
                             },
                         },
-                        { user: { id: loggedInUser.id } },
+                        {
+                            userId: loggedInUser.id,
+                        },
                     ],
                 },
                 orderBy: {
                     createdAt: 'desc',
                 },
-            });
-            console.log(result);
-            return result;
-        }),
+            })
+        ),
     },
 };
