428ab9c312a8794a90ff49379663dfca323e84d0
sannimdev <inegg.apps@gmail.com>
2022-03-06 21:35:40 +0900

    Upload Photo 대응

--- src/photos/uploadPhoto/uploadPhoto.resolvers.js
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,6 +1,7 @@
 import client from '../../client';
 import { protectedResolver } from '../../users/users.utils';
 import { processHashtags } from '../photo.utils';
+import { createWriteStream, existsSync, mkdirSync } from 'fs';
 
 export default {
     Mutation: {
@@ -12,10 +13,23 @@ export default {
                 hashtagObjs = processHashtags(caption);
             }
             //save the photo WITH the parsed hashtags
+            let fileUrl = null;
+            if (file) {
+                const {
+                    file: { filename, createReadStream },
+                } = file;
+                const savedPath = process.cwd() + '/uploads/' + loggedInUser.id;
+                const newFilename = `${Date.now()}${filename}`;
+                const readStream = createReadStream();
+                const writeStream = createWriteStream(savedPath + '/' + newFilename);
+                if (!existsSync(savedPath)) mkdirSync(savedPath);
+                readStream.pipe(writeStream);
+                fileUrl = `http://10.1.1.160:4001/static/${loggedInUser.id}/${newFilename}`;
+            }
             // add the photo to the hashtags
             return client.photo.create({
                 data: {
-                    file,
+                    file: fileUrl,
                     caption,
                     user: { connect: { id: loggedInUser.id } },
                     ...(hashtagObjs.length && {
